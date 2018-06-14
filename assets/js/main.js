57b41573c5ff246e535f07b83ba9563fbf1ac4a3
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-06-14 22:07:13 +0200

    A Needle in the Haystack

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,10 @@ function stringToArray(string){
     return string.split(' ');
 }
 
+// A Needle in the Haystack
+
+function findNeedle(haystack) {
+    for (var i = 0; i < haystack.length; i++) {
+        if (haystack[i] === 'needle') return 'found the needle at position ' + i;
+    }
+}
