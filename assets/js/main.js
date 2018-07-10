0435419350974f2413274e202e1b97ecccd2ae46
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-07-10 07:40:22 +0200

    add Multiples of 3 or 5 solution

--- assets/js/main.js
+++ assets/js/main.js
@@ -286,3 +286,15 @@ function order(words){
     }
     return result.join(" ");
 }
+
+// Multiples of 3 or 5
+
+function solution(number){
+    var sum = 0;
+    for(i = 0; i < number; i++){
+        if(i % 3 === 0 || i % 5 === 0){
+            sum = sum + i;
+        }
+    }
+    return sum
+}
